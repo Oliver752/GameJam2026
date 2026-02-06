cc82f470b158b961e1cf9a17842516855617e770
cloudy <klaudika.sz362@gmail.com>
2026-02-06 04:48:33 +0100

    effects on houses working

--- Assets/Scripts/mails/House.cs
+++ Assets/Scripts/mails/House.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 public class House : MonoBehaviour
 {
     bool playerNear;
+    bool spinning;
+    public ParticleSystem confettiPrefab;
 
-    public string[] reactions;
 
     void OnTriggerEnter(Collider other)
     {
@@ -20,19 +21,110 @@ public class House : MonoBehaviour
 
     void Update()
     {
+        Debug.Log("House Update running");
+
+        // doručenie listu
         if (playerNear && Input.GetKeyDown(KeyCode.F) && MailManager.Instance.HasMail())
+{
+    Debug.Log("DELIVER!");
+    React();
+    MailManager.Instance.currentMail = null;
+    UIManager.Instance.HideMail();
+}
+
+
+        // reality test spinning
+        if (spinning)
         {
-            React();
-            MailManager.Instance.currentMail = null;
-            UIManager.Instance.HideMail();
+            transform.Rotate(Vector3.up * 200 * Time.deltaTime);
         }
     }
 
     void React()
     {
-        int r = Random.Range(0, reactions.Length);
-        Debug.Log(reactions[r]);
+        switch (MailManager.Instance.currentType)
+        {
+            case MailType.Taxes:
+                ConfettiAndDisappear();
+                break;
 
-        transform.Rotate(0, 180, 0); // zatiaľ jednoduchá reakcia
+            case MailType.Nothing:
+                ConfettiRain();
+                break;
+
+            case MailType.NotYours:
+                transform.Rotate(0, 180, 0);
+                break;
+
+            case MailType.BecomeSomeone:
+                BecomeFlower();
+                break;
+
+            case MailType.Experiment:
+                FloatAway();
+                break;
+
+            case MailType.StopExisting:
+                gameObject.SetActive(false);
+                break;
+
+            case MailType.ChangeReality:
+    Camera.main.backgroundColor = Random.ColorHSV();
+    RenderSettings.fogColor = Random.ColorHSV();
+    break;
+
+
+            case MailType.Art:
+                GetComponent<Renderer>().material.SetFloat("_Glossiness", 1f);
+                break;
+
+            case MailType.Replacement:
+                RandomMove();
+                break;
+
+            case MailType.Shrink:
+                transform.localScale *= 0.5f;
+                break;
+
+            case MailType.Mistake:
+                transform.Rotate(180, 0, 0);
+                break;
+
+            case MailType.RealityTest:
+                spinning = true;
+                break;
+        }
     }
+
+    void BecomeFlower()
+    {
+        transform.localScale *= 0.3f;
+        GetComponent<Renderer>().material.color = Color.magenta;
+    }
+
+    void FloatAway()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.linearVelocity = Vector3.up * 5f;
+        Destroy(gameObject, 3f);
+    }
+
+    void RandomMove()
+    {
+        transform.position += new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5));
+    }
+
+    void ConfettiAndDisappear()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void ConfettiRain()
+{
+    ParticleSystem fx = Instantiate(confettiPrefab, transform.position + Vector3.up * 3f, Quaternion.identity);
+    fx.Play();
+    Destroy(fx.gameObject, 3f);
+}
+
 }
--- Assets/Scripts/mails/MailManager.cs
+++ Assets/Scripts/mails/MailManager.cs
@@ -1,26 +1,77 @@
 using UnityEngine;
 
+public enum MailType
+{
+    Taxes,
+    Nothing,
+    NotYours,
+    BecomeSomeone,
+    Experiment,
+    StopExisting,
+    ChangeReality,
+    Art,
+    Replacement,
+    Shrink,
+    Mistake,
+    RealityTest
+}
+
 public class MailManager : MonoBehaviour
 {
     public static MailManager Instance;
 
-    public string[] mails;
-
-    public string currentMail; // 🔥 TOTO CHÝBALO
+    public string currentMail;
+    public MailType currentType;
 
     void Awake()
     {
-        Instance = this;
-    }
-
-    public string GetRandomMail()
-    {
-        return mails[Random.Range(0, mails.Length)];
+        Instance = this;   // 🔥 TOTO CHÝBALO
     }
 
     public void TakeMail()
     {
-        currentMail = GetRandomMail();
+        int r = Random.Range(0, 12);
+        currentType = (MailType)r;
+
+        switch (currentType)
+        {
+            case MailType.Taxes:
+                currentMail = "Oops, you didn't pay your taxes.\nYour house will explode.\nSincerely, Nobody.";
+                break;
+            case MailType.Nothing:
+                currentMail = "Congratulations! You won nothing.";
+                break;
+            case MailType.NotYours:
+                currentMail = "This is not your mail.\nBut open it anyway.";
+                break;
+            case MailType.BecomeSomeone:
+                currentMail = "You will now become someone else.";
+                break;
+            case MailType.Experiment:
+                currentMail = "Your house was selected for a random experiment.";
+                break;
+            case MailType.StopExisting:
+                currentMail = "Please stop existing.";
+                break;
+            case MailType.ChangeReality:
+                currentMail = "We changed your reality. Sorry.";
+                break;
+            case MailType.Art:
+                currentMail = "Your house is now a piece of art.";
+                break;
+            case MailType.Replacement:
+                currentMail = "Your mail was lost. This is a replacement.";
+                break;
+            case MailType.Shrink:
+                currentMail = "Warning: your house is shrinking.";
+                break;
+            case MailType.Mistake:
+                currentMail = "Sorry, we made a mistake.";
+                break;
+            case MailType.RealityTest:
+                currentMail = "This is a reality test.";
+                break;
+        }
     }
 
     public bool HasMail()
