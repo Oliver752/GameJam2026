31c672cb77e877e5485edea501ce7a0b30332d31
SimonaPoliakova <poliakova27712@gmail.com>
2026-02-06 07:39:55 +0100

    funkcne buttony v main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("map");
+    }
+
+    public void OpenTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
